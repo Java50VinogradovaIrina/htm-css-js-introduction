9dee0caf682a9d531a1b87fb2e14604639c407c9
Irina <vinogradova8998@gmail.com>
2022-12-13 21:25:57 +0200

    lesson #8

---
+++ src/flowers.js
@@ -0,0 +1,13 @@
+const detailsImage = document.querySelector("details-image");
+const detailsTitle = document.querySelector("details-title");
+const anchorElements =document.querySelectorAll("thambnails-anchor")
+
+function setDetails(anchor) {
+    detailsImage.src = anchor.getAttribute(data-details-image);
+    detailsTitle.innerHTML = anchor.getAttribute(data-details-title);
+}
+for(let i =0; i<anchorElements.length; i++) {
+    anchorElements[i].addEventListener("click",function(){
+        setDetails(anchorElements[i]);
+    })
+}
